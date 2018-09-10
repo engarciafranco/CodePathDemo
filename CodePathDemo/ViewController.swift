9ea7b035ca1a13ef8489c08eff725f7b8f2a42f0
Enrique Garcia <engarciafranco@enriques-mbp.lan>
2018-09-09 21:36:52 -0500

    created the base for the pre-work app, and added .gitignore, .gitattributes, and README file

--- CodePathDemo/ViewController.swift
+++ CodePathDemo/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLable: UILabel!
+    
+    var backgroundColor: UIColor!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        backgroundColor = view.backgroundColor
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func didTapButton(_ sender: Any) {
+        print("Hello")
+        textLable.textColor = UIColor.orange
+    }
 
+    @IBAction func didTapviewButton(_ sender: Any) {
+        view.backgroundColor = UIColor.blue
+    }
+    
+    @IBAction func didTapTextbutton(_ sender: Any) {
+        textLable.text = "Goodbye!"
+    }
+    
+    @IBAction func onResetGesture(_ sender: Any) {
+        textLable.text = "Hello"
+        view.backgroundColor = backgroundColor
+    }
+    
+    
 }
 
