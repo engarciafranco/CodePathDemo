c479a0fc6e2aa2ea207c15ea14d56fc708f472ea
Enrique Garcia <engarciafranco@enriques-mbp.lan>
2018-09-10 21:40:17 -0500

    check off all required and Optional user stories (i.e. implimented Optional stories 1-4

--- CodePathDemo/ViewController.swift
+++ CodePathDemo/ViewController.swift
@@ -11,15 +11,17 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var textLable: UILabel!
-    
+    @IBOutlet weak var helperTextLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
     var backgroundColor: UIColor!
+    var textColor: UIColor!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
         backgroundColor = view.backgroundColor
-        
+        textColor = textLable.textColor
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,22 +29,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    // Change Text Color
     @IBAction func didTapButton(_ sender: Any) {
-        print("Hello")
-        textLable.textColor = UIColor.orange
+        textLable.textColor = UIColor.init(red: 224/255, green: 26/255, blue: 79/255, alpha: 1)
+        helperTextLabel.isHidden = true
     }
 
+     /* Change Background Color
+     Additionally:
+        - Change the button color to match with the lighter background color.
+     */
     @IBAction func didTapviewButton(_ sender: Any) {
-        view.backgroundColor = UIColor.blue
+        view.backgroundColor = UIColor.init(red: 211/255, green: 250/255, blue: 214/255, alpha: 1)
+        helperTextLabel.isHidden = true
     }
     
+    // Change Label text
     @IBAction func didTapTextbutton(_ sender: Any) {
-        textLable.text = "Goodbye!"
+       
+        //Get text from textField
+        
+        if (textField.text == "") {
+           textLable.text = "Goodbye 👋."
+        } else {
+            textLable.text = textField.text
+            textField.text = ""
+            view.endEditing(true)
+        }
+        
+        helperTextLabel.isHidden = true
     }
     
+    // Reset All
     @IBAction func onResetGesture(_ sender: Any) {
-        textLable.text = "Hello"
+        textLable.text = "Hello from Enrique!"
         view.backgroundColor = backgroundColor
+        textLable.textColor = textColor
+        helperTextLabel.isHidden = false
     }
     
     
